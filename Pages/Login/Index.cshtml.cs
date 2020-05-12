7fbd01fa93800aad0987450695b2c56d7b38f6c6
Sergio Paz <alejandroax@live.com.mx>
2020-05-11 22:53:52 -0600

    Completing Login form

--- Pages/Login/Index.cshtml.cs
+++ Pages/Login/Index.cshtml.cs
@@ -4,22 +4,56 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUDCore.Models;
+
 
 namespace CRUDCore.Pages.Login
 {
     public class IndexModel : PageModel
     {
-       
+        public bool showListUsers { get; set; }
 
+        private readonly CRUDCore.Models.CRUDAPPContext _context;
+
+        public IndexModel(CRUDCore.Models.CRUDAPPContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CtUsers CtUsers { get; set; }
+
+        [BindProperty]
+        public string UsuarioSelected { get; set; }
+        public List<SelectListItem> CategoryItems { set; get; }
+
+       
+       
         public void OnGet()
         {
 
         }
 
 
-        public void OnPost(string txtUsuario, string txtPassword)
+        public IActionResult OnPost(string txtUsuario, string txtPassword)
         {
-            string pepe = "asd";
+            showListUsers = false;
+            if (txtUsuario=="spaz")
+            {
+                CategoryItems = _context.CtUsers.Select(a => new SelectListItem
+                               {
+                                   Value = a.UserName,
+                                   Text = a.UserName
+                               })
+                    .ToList();
+
+                showListUsers = true;
+              
+            }
+           
+            return Page();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUDCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,7 +33,7 @@ namespace CRUDCore
             });
 
             //var connection = @"Server=DESKTOP-7VHO3GN\SQLEXPRESS;Database=CRUDAPP;Trusted_connection=True";
-
+            services.AddScoped<CRUDAPPContext, CRUDAPPContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
